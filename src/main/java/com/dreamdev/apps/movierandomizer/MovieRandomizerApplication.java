ce89962f816dd7415c8a2cb45f8ce7e0279faa2a
Nan Ye <ynyz2003@hotmail.com>
2025-08-03 21:13:02 -0500

    init

---
+++ src/main/java/com/dreamdev/apps/movierandomizer/MovieRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.dreamdev.apps.movierandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRandomizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieRandomizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamdev/apps/movierandomizer/MovieRandomizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamdev.apps.movierandomizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRandomizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
